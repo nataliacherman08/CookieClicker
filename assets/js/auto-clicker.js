bb6268fdb4d5cb72032381fa743349a0af02ab2a
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-02-22 17:47:55 +0100

    js done

--- assets/js/auto-clicker.js
+++ assets/js/auto-clicker.js
@@ -1,36 +1,44 @@
 window.onload = () => {
-    let incrementation= 0;
-    let addition = 0;
-    let score = 0;
-    let multiplifier = 1; 
+    let score = 0.0; //score
+    let incrementation= 0.0; // c'est la valuer qui va s'ajouter
+    let multiplifier = 1; // multiplie le click
+    let priceAutoClicker = 25;
+    let priceButtonX2 = 500; // prix initial du bouton X2
 
-    let priceButtonX2 = 500;
 
-
-    const scoreTitle = document.getElementById("score")
+    const scoreTitle = document.getElementById("score");
     const autoClicker = document.getElementById("auto-clicker");
     const buttonX2 = document.getElementById("buttonX2");
+    
 
-
+    disabled()
+    // auto-clicker event
     autoClicker.addEventListener("click", () =>{
-        incrementation += 1;
-        autoClicker.children[2].innerHTML = parseInt(autoClicker.children[2].innerHTML) + 1
+        incrementation += 0.1; // increse the incrementation in the set interval
+        minScore(priceAutoClicker);
+        // modif the button
+        autoClicker.children[2].innerHTML = parseInt(autoClicker.children[2].innerHTML) + 1;
+        priceAutoClicker = priceAutoClicker + (priceAutoClicker *0.2);
+        autoClicker.children[1].innerHTML = parseInt(priceAutoClicker);
+        disabled()
     })
 
-
+    // on click
     document.getElementById("click").addEventListener("click", () =>{
-        score = parseInt(scoreTitle.innerHTML) +multiplifier;
-        scoreTitle.innerHTML = score
+        score = parseFloat(scoreTitle.innerHTML) +multiplifier;
+        scoreTitle.innerHTML = score.toFixed(1);
+        disabled()
     })
+
 
 
 
     // --------------------------------
-
     setInterval( () =>{
-        score = parseInt(scoreTitle.innerHTML) + incrementation
-        scoreTitle.innerHTML = score
-    }, 2000)
+        score = parseFloat(scoreTitle.innerHTML) + incrementation;
+        scoreTitle.innerHTML = score.toFixed(1);
+        disabled()
+    }, 100)
 
 
 
@@ -39,6 +47,22 @@ window.onload = () => {
         priceButtonX2 = priceButtonX2 * 2; 
         // changer dans le html
         multiplifier = multiplifier * 2; 
-        console.log(multiplifier)
+        console.log(multiplifier);
     })
+
+    function disabled(){    
+        let buttonClick = document.querySelectorAll(".clickModif")
+        buttonClick.forEach(button =>{
+            if (parseFloat(button.children[1].innerHTML) <= parseFloat(score)){
+                button.disabled = false;
+            }
+            else{
+                button.disabled = true;
+            }
+        })
+    }
+    function minScore(minus){
+        scoreTitle.innerHTML = parseFloat(scoreTitle.innerHTML) - minus
+    }
 }
+
