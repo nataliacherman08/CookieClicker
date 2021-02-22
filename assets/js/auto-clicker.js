bc541c0876e8963ca1b1551a7bb95d23a58acf15
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-02-22 14:58:27 +0100

    init

--- assets/js/auto-clicker.js
+++ assets/js/auto-clicker.js
@@ -1,12 +1,44 @@
-let score = 0;
-let incrementation= 0;
-let addition = 0;
-document.getElementById("auto-clicker").addEventListener("click", () =>{
-    incrementation += 1;
-})
-setInterval( () =>{
+window.onload = () => {
+    let incrementation= 0;
+    let addition = 0;
+    let score = 0;
+    let multiplifier = 1; 
 
-    let test = document.getElementById("score")
-    addition = parseInt(test.innerHTML) + incrementation
-    test.innerHTML = addition
-},100)
+    let priceButtonX2 = 500;
+
+
+    const scoreTitle = document.getElementById("score")
+    const autoClicker = document.getElementById("auto-clicker");
+    const buttonX2 = document.getElementById("buttonX2");
+
+
+    autoClicker.addEventListener("click", () =>{
+        incrementation += 1;
+        autoClicker.children[2].innerHTML = parseInt(autoClicker.children[2].innerHTML) + 1
+    })
+
+
+    document.getElementById("click").addEventListener("click", () =>{
+        score = parseInt(scoreTitle.innerHTML) +multiplifier;
+        scoreTitle.innerHTML = score
+    })
+
+
+
+    // --------------------------------
+
+    setInterval( () =>{
+        score = parseInt(scoreTitle.innerHTML) + incrementation
+        scoreTitle.innerHTML = score
+    }, 2000)
+
+
+
+    //       -------------------------
+    buttonX2.addEventListener("click", () =>{
+        priceButtonX2 = priceButtonX2 * 2; 
+        // changer dans le html
+        multiplifier = multiplifier * 2; 
+        console.log(multiplifier)
+    })
+}
